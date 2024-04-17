8c009edcc9121f78bd221cb71fea917352677324
Muhais Olatundun <monnkcode@gmail.com>
2024-04-16 19:29:56 -0500

    Feature added

--- main.py
+++ main.py
@@ -13,17 +13,20 @@ count = 0
 while guess != secret_number:
     user_input = input("Guess the secret number between 1 and 100: ")
 
-    # Check if the input is a valid number
+     #this Checks if the input is a valid number
     if user_input.isdigit():
         guess = int(user_input)
         count += 1
-
+            
         if guess < secret_number:
             print("Too low!! Try Again")
         elif guess > secret_number:
             print("Too high! Try again.")
     else:
         print("Please enter a valid number.")
-
+            
 print(f"Congratulations! You guessed the secret number {secret_number} in {count} tries.")
-
+if count < 5:
+    print("Fast thinker")
+else:
+    print("took you awhile")
